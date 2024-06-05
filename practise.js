4f7fdddf1322709756e00ce6f2dee8fa2e06cb60
AlphaAnas <muhd.anas70pe@gmail.com>
2024-06-05 23:10:37 +0500

    Added folder of number guessing game

---
+++ practise.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "Q1",
+        type: "input",
+        message: "What is your name ? ",
+    },
+    {
+        name: "Q2",
+        type: "list",
+        message: "What is your gender ? ",
+        choices: ["Male", "Female"],
+    },
+]);
+console.log(answer);
+console.log("Hello", answer.Q1);
+console.log(`Your gender is ${answer.Q2}`);
+let color = "blue";
+let number = 123.1111111111;
+console.log(number.toFixed(4));
+let num1 = 18;
+console.log(typeof num1);
+//narrowing
+let newAge = Math.random() > 0.6 ? 18 : "Hamza";
+if (typeof newAge === "number") {
+    console.log(newAge.toFixed(10));
+}
+else {
+    console.log(newAge.toUpperCase());
+}
