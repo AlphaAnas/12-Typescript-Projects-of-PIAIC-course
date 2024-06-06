405b62853f66514b9201c50d32c041496d95a986
AlphaAnas <muhd.anas70pe@gmail.com>
2024-06-06 12:30:46 +0500

    game making in progress

---
+++ Number_Guessing_Game/index.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+
+while (true) {
+  const answers = await inquirer.prompt([
+    {
+      name: "userGuessedNumber",
+      type: "input",
+      message: "Guess a number between 1 and 10: ",
+    },
+  ]);
+
+  if (answers.userGuessedNumber == randomNumber) {
+    console.log("You guessed right!! ");
+  } else {
+    console.error("You guessed wrong!");
+  }
+}
