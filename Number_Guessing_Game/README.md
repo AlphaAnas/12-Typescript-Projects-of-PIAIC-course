f4244f7b883518361121af214c91d853133b1f50
AlphaAnas <muhd.anas70pe@gmail.com>
2024-06-06 14:01:24 +0500

    final version published

--- Number_Guessing_Game/README.md
+++ Number_Guessing_Game/README.md
@@ -6,21 +6,35 @@ A simple number guessing game implemented in TypeScript.
 
 1. Clone the repository:
    ```sh
-   git clone https://github.com/yourusername/number-guessing-game.git
-   cd number-guessing-game
+   git clone https://github.com/AlphaAnas/Typescript-Projects-PIAIC-course.git
+   or download the zip folder
    ```
 2. Install dependencies:
-   ```sh
-   npm install
+   First Open the folder in VS-Code
+   then in the terminal type "cd Number_Guessing_Game"
+   to get to the the current Number Guessing Game folder
+
+   ```
+
+   npm install (to install node modules folder)
+   npm install inquirer
+   npm --init (to install package.json)
+   tsc init   (to install tsconfig.json)
+
    ```
 
 ## Usage
 
 Start the game:
+To just play the game you can install it using your terminal and play it with the command :
+`npm i @alpha_anas/number_guessing_game`
+
+and then type
+`npx @alpha_anas/number_guessing_game`
 
 ```sh
-npm start
-Game Rules
+
+Game Rules:
 Guess the randomly generated number within the range.
 Feedback provided for each guess (too high, too low, correct).
 Limited number of attempts to guess correctly.
--- Number_Guessing_Game/index.ts
+++ Number_Guessing_Game/index.ts
@@ -1,20 +1,65 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 const randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log(randomNumber);
 
-while (true) {
-  const answers = await inquirer.prompt([
-    {
-      name: "userGuessedNumber",
-      type: "input",
-      message: "Guess a number between 1 and 10: ",
-    },
-  ]);
+const main = async (total: number, randomNumber: number) => {
+  let attempts = 0;
 
-  if (answers.userGuessedNumber == randomNumber) {
-    console.log("You guessed right!! ");
-  } else {
-    console.error("You guessed wrong!");
+  while (true) {
+    console.log("Attempts left : ", total - attempts);
+    if (attempts == total) {
+      console.log(chalk.red.bold("You have exhausted all your attempts!"));
+      console.log(chalk.green.bold(`The number was ${randomNumber}`));
+      break;
+    }
+
+    const answers = await inquirer.prompt([
+      {
+        name: "userGuessedNumber",
+        type: "input",
+        message: "Guess a number between 1 and 10: ",
+      },
+    ]);
+
+    if (answers.userGuessedNumber == randomNumber) {
+      console.log(chalk.green.bold("You guessed right!! "));
+      break;
+    } else if (isNaN(answers.userGuessedNumber)) {
+      console.error(
+        chalk.yellow(
+          "Invalid data type. Please enter a number between 1 and 10"
+        )
+      );
+      continue;
+    } else {
+      if (answers.userGuessedNumber > 10 || answers.userGuessedNumber < 1) {
+        console.error(chalk.yellow("Please enter a number between 1 and 10"));
+        console.log(" ");
+        continue;
+      }
+      console.error(chalk.red.bold("You guessed wrong!"));
+      if (answers.userGuessedNumber > randomNumber) {
+        console.log("Too High");
+      } else if (answers.userGuessedNumber < randomNumber) {
+        console.log("Too Low");
+      }
+      attempts += 1;
+      if (total - attempts == 1) {
+        console.log(
+          chalk.green(
+            `The number is between ${randomNumber - 3} and ${randomNumber + 2}`
+          )
+        );
+      }
+    }
   }
-}
+};
+const total = 5;
+console.log("******* Welcome to the Number guessing Game !   *************");
+console.log();
+console.log(`You have ${total} attempts to guess the number between 1 and 10`);
+
+main(total, randomNumber);
